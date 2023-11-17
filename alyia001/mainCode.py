8d5944d8e9e2ef0448792f8c078b756227bca3e4
Alyia001 <133720068+Alyia001@users.noreply.github.com>
2023-11-17 20:53:42 +1030

    class Alchemist implementation

--- alyia001/mainCode.py
+++ alyia001/mainCode.py
@@ -11,25 +11,27 @@
 
 
 class Alchemist:
-    def __init__(self, attack, strength, defense, magic, range, necromancy):
-        self.attack = attack
-        self.strength = strength
-        self.defense = defense
-        self.magic = magic
-        self.range = range
-        self.necromancy = necromancy
+    def __init__(self):
+        self.attack = 0
+        self.strength = 0
+        self.defence = 0
+        self.magic = 0
+        self.ranging = 0
+        self.necromancy = 0
 
-    def mix_potion(self, ingredient1, ingredient2):
-        pass
+    def mix_potion(self, laboratory, ingredient1, ingredient2):
+        potion = laboratory.mix_potion(ingredient1, ingredient2)
+        if potion:
+            self.drink_potion(potion)
 
     def drink_potion(self, potion):
-        pass
+        setattr(self, potion.stat_attribute, getattr(self, potion.stat_attribute) + potion.calculate_boost())
 
-    def collect_reagent(self, reagent):
-        pass
+    def collect_reagent(self, laboratory, reagent):
+        laboratory.add_reagent(reagent)
 
-    def refine_reagents(self):
-        pass
+    def refine_reagents(self, laboratory):
+        laboratory.refine_reagents()
 
 
 class Laboratory:
