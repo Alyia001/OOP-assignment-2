5d2750a6b4aba35fda0676ac78573aebcc527f30
Alyia001 <133720068+Alyia001@users.noreply.github.com>
2023-11-17 21:05:01 +1030

    Setting up test code for potion calc and mixing

--- alyia001/testCode.py
+++ alyia001/testCode.py
@@ -7,3 +7,30 @@
 
 import unittest
 from mainCode import Potion, Reagent, Laboratory, Alchemist
+
+class TestAlchemist(unittest.TestCase):
+    def test_potion_calculation_super_potion(self):
+        herb = Reagent("Irit", 1.0)
+        catalyst = Reagent("Eye of Newt", 4.3)
+        super_attack_potion = Potion("Super Attack", "attack", 0)
+
+        pass
+
+    def test_potion_calculation_extreme_potion(self):
+        herb = Reagent("Irit", 1.0)
+        catalyst = Reagent("Eye of Newt", 4.3)
+        super_attack_potion = Potion("Super Attack", "attack", 0)
+        extreme_attack_potion = Potion("Extreme Attack", "attack", 0)
+
+        pass
+
+    def test_potion_mixing(self):
+        laboratory = Laboratory()
+        herb = Reagent("Irit", 1.0)
+        catalyst = Reagent("Eye of Newt", 4.3)
+        super_attack_potion = Potion("Super Attack", "attack", 0)
+
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
