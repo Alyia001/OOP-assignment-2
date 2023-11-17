77c1db1f86da0ace2730e9e007c9e06d2a45e3ef
Alyia001 <133720068+Alyia001@users.noreply.github.com>
2023-11-17 20:35:54 +1030

    Header implementation

--- alyia001/mainCode.py
+++ alyia001/mainCode.py
@@ -1,3 +1,15 @@
+#File: mainCode.py
+#Description: Main Code for the implementation of a common RPG uml design
+#Author: Ian Amir Al Faily
+#StudentID:110416702
+#EmailID: Alyia001
+#This is my own work as defined by the University's Academic Misconduct Policy.
+
+
+
+
+
+
 class Alchemist:
     def __init__(self, attack, strength, defense, magic, range, necromancy):
         self.attack = attack
