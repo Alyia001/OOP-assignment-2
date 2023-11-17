c427f697f4f8c7dc58787a141bf3e0646c8280e8
Alyia001 <133720068+Alyia001@users.noreply.github.com>
2023-11-17 21:05:45 +1030

    Implementation of testing code

--- alyia001/testCode.py
+++ alyia001/testCode.py
@@ -14,7 +14,8 @@ class TestAlchemist(unittest.TestCase):
         catalyst = Reagent("Eye of Newt", 4.3)
         super_attack_potion = Potion("Super Attack", "attack", 0)
 
-        pass
+        super_attack_potion.calculate_boost(herb)
+        self.assertEqual(super_attack_potion.boost_attribute, 5.45)  # Adjust based on your calculation logic
 
     def test_potion_calculation_extreme_potion(self):
         herb = Reagent("Irit", 1.0)
@@ -22,7 +23,9 @@ class TestAlchemist(unittest.TestCase):
         super_attack_potion = Potion("Super Attack", "attack", 0)
         extreme_attack_potion = Potion("Extreme Attack", "attack", 0)
 
-        pass
+        super_attack_potion.calculate_boost(herb)
+        extreme_attack_potion.calculate_boost(super_attack_potion)
+        self.assertEqual(extreme_attack_potion.boost_attribute, 16.35)  # Adjust based on your calculation logic
 
     def test_potion_mixing(self):
         laboratory = Laboratory()
@@ -30,7 +33,10 @@ class TestAlchemist(unittest.TestCase):
         catalyst = Reagent("Eye of Newt", 4.3)
         super_attack_potion = Potion("Super Attack", "attack", 0)
 
-        pass
+        laboratory.add_reagent(herb)
+        laboratory.add_reagent(catalyst)
+        laboratory.mix_potion(herb, super_attack_potion)
+        self.assertIn(super_attack_potion, laboratory.potions)
 
 if __name__ == '__main__':
     unittest.main()
